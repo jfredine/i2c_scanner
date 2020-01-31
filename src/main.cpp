06ccef8ffac0caeb07f4a293c5cfa23d21eca367
John Fredine <jfredine@hotmail.com>
2020-01-30 21:38:27 -0800

    Initial version

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+
+void setup() {
+    Serial.begin(9600);
+#ifdef __AVR_ATmega328P__
+    Wire.begin();
+#else
+    Wire.begin(2, 14);
+#endif
+
+    Serial.println("Begin I2C bus scan ...");
+    for (byte i = 8; i < 127; i++) {
+        Wire.beginTransmission(i);
+        if (Wire.endTransmission() == 0) {
+            Serial.print("Found device @ 0x");
+            Serial.println(i, HEX);
+            delay(1);
+        }
+    }
+    Serial.println("I2C bus scan complete.");
+}
+
+void loop() {
+}
